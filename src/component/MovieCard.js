6b05ec2c4fcc3c26325655b2bb156f48ea8c7f1a
Sharad Jain <sharad.jain@photon.com>
2024-02-19 15:09:07 +0530

    created movie card details popup
    created movie card details popup

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import { POSTER_IMG_PATH } from '../utils/constants'
+import React from "react";
+import { POSTER_IMG_PATH } from "../utils/constants";
+import ImagePopup from "./ImagePopup";
+import { useState } from "react";
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({
+  posterPath,
+  overview,
+  title,
+  release_date,
+  vote_average,
+}) => {
+  const [popupTrigger, setPopupTrigger] = useState(false);
+  const handlePopupTrigger = () => {
+    setPopupTrigger(!popupTrigger);
+  };
+  const getReleaseDate = new Date(release_date).toLocaleDateString();
+
   return (
-    <div className='w-44 pr-4'>
-        <img src={POSTER_IMG_PATH + posterPath} alt='movie poster' />
+    <div className="w-44 pr-4">
+      <ImagePopup trigger={popupTrigger} setTrigger={setPopupTrigger}>
+        <div className=" flex text-white max-w-[700px] m-auto p-8 bg-red-700 rounded-sm bg-opacity-70 relative">
+          <div className="px-2 w-1/3">
+            <img src={POSTER_IMG_PATH + posterPath} alt="movie poster" />
+          </div>
+          <div className="px-2 w-2/3">
+            <h3 className="mb-4 text-2xl">{title}</h3>
+            <p className="mb-2">{overview}</p>
+            <p className="mb-2">
+              Average Ratings: {Math.round(vote_average)}/10
+            </p>
+            <p className="">Release Date: {getReleaseDate}</p>
+          </div>
+        </div>
+      </ImagePopup>
+      <div onClick={handlePopupTrigger} className=" cursor-pointer">
+        <img src={POSTER_IMG_PATH + posterPath} alt="movie poster" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/component/MoviesList.js
+++ src/component/MoviesList.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React from "react";
+import MovieCard from "./MovieCard";
 
-const MoviesList = ({title, movies}) => {
+const MoviesList = ({ title, movies }) => {
   return (
-    <div className='px-6 overflow-hidden pb-10'>
-        <h2 className='text-left py-6 text-2xl font-bold text-white'>{title}</h2>
-        <div className='flex overflow-x-auto'>
-            <div className='flex'>
-                {
-                    movies?.map(movie => {
-                        return <MovieCard key={movie.id} posterPath={movie.poster_path} />
-                    })
-                }
-            </div>
+    <div className="px-6 overflow-hidden pb-10">
+      <h2 className="text-left py-6 text-2xl font-bold text-white">{title}</h2>
+      <div className="flex overflow-x-auto">
+        <div className="flex">
+          {movies?.map((movie) => {
+            return (
+              <MovieCard
+                key={movie.id}
+                posterPath={movie.poster_path}
+                overview={movie.overview}
+                title={movie.title}
+                release_date={movie.release_date}
+                vote_average={movie.vote_average}
+              />
+            );
+          })}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MoviesList
+export default MoviesList;
